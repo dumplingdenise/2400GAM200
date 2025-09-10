d2d42409023ed2a33b62df2eaf6781f00f715e7e
Denise <deniseteo02@gmail.com>
2025-09-10 22:38:16 +0800

    Added scripts

---
+++ GAM200_Prototype/Assets/Scripts/gameController.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    /* This script is for handling of the game session.
+        Use for game state -> running, paused, gameOver etc.
+        Stores & Restore checkpoints (**TBC if script separated or not yet)
+        Handles respawning when player die (**TBC if script separated or not yet)
+        Control UI Menus (**TBC if script separated or not yet)    
+     */
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GAM200_Prototype/Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    /* For player state, input & control
+        Player movement
+        Player input
+        Player state (if have)
+     */
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
