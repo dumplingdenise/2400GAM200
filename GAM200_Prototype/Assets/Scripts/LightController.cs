f8acc3c0c4cab4970b57e9fc35161d33322bc71c
Denise <deniseteo02@gmail.com>
2025-09-12 00:29:37 +0800

    Re setup of the scripts and scenes

---
+++ GAM200_Prototype/Assets/Scripts/LightController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    /*
+        Central system for handling the light mechanics. 
+        Track and tells the shadowCasters / shadowObjects script how to behave, what to do
+     */
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
