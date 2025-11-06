64090de63c86c3e17c8621cc91edf9529472dbab
Denise <deniseteo02@gmail.com>
2025-11-06 19:29:48 +0800

    light controller

--- GAM200_Prototype/Assets/Scripts/LightController.cs
+++ GAM200_Prototype/Assets/Scripts/LightController.cs
@@ -63,7 +63,7 @@ public class LightController : MonoBehaviour
         {
             UpdateAllShadows();
 
-            SetShadowsGhostMode(true);
+            SetShadowsGhostMode(!isFrozen);
         }  
     }
 
